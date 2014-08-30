e08821b8e9b4cc55cb1279b1d7eeb0bb64b3fcb6
Lucasargh <lucasargh@gmail.com>
2014-08-30 11:58:43 -0700

    Adding Basic Auth
    This authentication is incredibly basic and incredibly insecure. Once we have the database setup we can update it to use hashed/salted passwords and a username database.

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'activerecord'
 
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+	username == 'little_jimmy' and password = 'jimmyisthegreatest'
+end
+
 get '/' do
 	erb views/index.html
 end
